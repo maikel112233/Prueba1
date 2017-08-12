e6f6e93ed3af528c22c508f8ec79d15e2709b4b4
maikel112233 <maikel112233@gmail.com>
2017-08-12 12:42:56 +0200

    asistente de nuevo proyecto

--- app/src/main/java/com/imprimir4d/prueba1/MainActivity.java
+++ app/src/main/java/com/imprimir4d/prueba1/MainActivity.java
@@ -2,4 +2,13 @@ package com.imprimir4d.prueba1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hola que tal
+    }
+}
